74773c2ff2fa9c30e482bd8fc8a8413e6f878016
Dami500 <damilareajayiobe0@gmail.com>
2024-12-18 08:36:26 -0500

    Update

--- ensemble_prediction.py
+++ ensemble_prediction.py
@@ -2,9 +2,7 @@ import datetime
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import yfinance as yf
 import seaborn as sns
-import sklearn
 from sklearn.ensemble import BaggingRegressor, RandomForestRegressor, AdaBoostRegressor
 from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split
@@ -80,6 +78,55 @@ if __name__ == '__main__':
     start = datetime.datetime(2006, 1, 1)
     end = datetime.datetime(2015, 12, 31)
     amzn = obtain_lagged_series("AMZN", start, end, lags=3)
-    print(amzn)
+    # Use the first three daily lags of AMZN closing prices and scale the data to lie within -1 and +1 for comparison
+    X = amzn[["lag1", "lag2", "lag3"]]
+    y = amzn["direction"]
+    X = scale(X)
+    y = scale(y)
+    # Use the training-testing split with 70% of data in the
+    # training data with the remaining 30% of data in the testing
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=random_state)
+    # Pre-create the arrays which will contain the MSE for each particular ensemble method
+    estimators = np.zeros(axis_step)
+    bagging_mse = np.zeros(axis_step)
+    rf_mse = np.zeros(axis_step)
+    boosting_mse = np.zeros(axis_step)
+    # Estimate the Bagging MSE over the full number of estimators, across a step size ("step_factor")
+    for i in range(0, axis_step):
+        print("Bagging Estimator: %d of %d..." % (step_factor * (i + 1), n_estimators))
+        bagging = BaggingRegressor(DecisionTreeRegressor(), n_estimators=step_factor * (i + 1), n_jobs=n_jobs,
+        random_state=random_state)
+        bagging.fit(X_train, y_train)
+        mse = mean_squared_error(y_test, bagging.predict(X_test))
+        estimators[i] = step_factor * (i + 1)
+        bagging_mse[i] = mse
+    # Estimate the Random Forest MSE over the full number of estimators, across a step size ("step_factor")
+    for i in range(0, axis_step):
+        print("Random Forest Estimator: %d of %d..." % (step_factor * (i + 1), n_estimators))
+        rf = RandomForestRegressor(n_estimators=step_factor * (i + 1), n_jobs=n_jobs, random_state=random_state)
+        rf.fit(X_train, y_train)
+        mse = mean_squared_error(y_test, rf.predict(X_test))
+        estimators[i] = step_factor * (i + 1)
+        rf_mse[i] = mse
+    # Estimate the AdaBoost MSE over the full number of estimators, across a step size ("step_factor")
+    for i in range(0, axis_step):
+        print("Boosting Estimator: %d of %d..." % (step_factor * (i + 1), n_estimators))
+        boosting = AdaBoostRegressor(DecisionTreeRegressor(), n_estimators=step_factor * (i + 1),
+                                     random_state=random_state,learning_rate=0.01)
+        boosting.fit(X_train, y_train)
+        mse = mean_squared_error(y_test, boosting.predict(X_test))
+        estimators[i] = step_factor * (i + 1)
+        boosting_mse[i] = mse
+
+    # Plot the chart of MSE versus number of estimators
+    plt.figure(figsize=(8, 8))
+    plt.title('Bagging, RandomForest and Boosting comparison')
+    plt.plot(estimators, bagging_mse, 'b -', color = "black", label ='Bagging')
+    plt.plot(estimators, rf_mse, 'b -', color = "blue", label ='RandomForest')
+    plt.plot(estimators, boosting_mse, 'b -', color = "red", label ='AdaBoost')
+    plt.legend(loc='upper right')
+    plt.xlabel('Estimators')
+    plt.ylabel('Mean Squared Error')
+    plt.show()
 
 
